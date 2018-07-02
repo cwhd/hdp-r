f9ec26185465a51b1b56037ba4a6829620fa4bf1
Christopher Davis <chrdavi@microsoft.com>
2018-07-02 14:57:39 -0700

    adding functions and documents

--- hdpr/R/modelManager.R
+++ hdpr/R/modelManager.R
@@ -1,33 +1,35 @@
 # manage input and output of models
-#
-# models are represented in JSON like this:
-#{
-#  "title": "model title",
-#  "levels": [{
-#    "level": 1,
-#    "title": "level 1 title: Mission",
-#    "description": "level 1 description",
-#    "nodes": [{
-#      "name": "select best technology"
-#    }]
-#  },
-#  {
-#    "level": 2,
-#    "title": "level 2 title: Objective",
-#    "description": "level 2 description",
-#    "nodes": [{
-#      "name": "Cost",
-#      "description": "how much stuff costs"
-#    }]
-#  }
-#}
+# uses the rjson lib: https://cran.r-project.org/web/packages/rjson/index.html
+require(rjson)
 
-hdp.model.import <- function() {
+#' Import HDP Models into R from JSON
+#'
+#' This function allows you to import an HDP model defined in JSON. Please
+#' check out th documentation on github.
+#' @param json_file The file you're importing.
+#' @keywords json, hdp, hdm
+#' @export
+#' @examples
+#' hdp.model.import("hdpModel.json")
+hdp.model.import <- function(jsonFile) {
   print("importing...")
-  #https://cran.r-project.org/web/packages/rjson/index.html
-  #json_data <- fromJSON(file=json_file)
+  json_data <- fromJSON(file=jsonFile)
+  return(json_data)
+  #print(json_data)
 }
 
-hdp.model.export <- function() {
-  print("exporting...");
+#' Export HDP Models from R to JSON
+#'
+#' This function allows you to export an HDP model as a JSON file. Please
+#' check out th documentation on github.
+#' @param hdpModel An object representing your heirarchical model
+#' @param file_name Name of the file to save. Will append .json to the end of it.
+#' @keywords json, hdp, hdm
+#' @export
+#' @examples
+#' hdp.model.export(hdpModel, "hdpModel.json")
+hdp.model.export <- function(hdpModel, fileName) {
+  print(paste("saving to ", fileName))
+  jsonOutput <- toJSON(hdpMod)
+  write(jsonOutput, paste(fileName, ".json"))
 }
